d3eab2a065d4df647bf17e983083edeb4cb79af2
Tofu <kmyeongb@gmail.com>
2022-08-24 12:00:53 +0900

    object

--- ch06/src/Overloading.java
+++ ch06/src/Overloading.java
@@ -39,9 +39,54 @@
 public class Overloading {
 
 	public static void main(String[] args) {
+		Math3 mm = new Math3();
+
+//		add메서드가 println메서드보다 먼저 실행된다.
+//		int result = mm.add(3, 3);
+//		System.out.println("mm.add(3, 3) 결과:" + result);
 		
-		
+		System.out.println("mm.add(3, 3) 결과:"	 + mm.add(3, 3));
+		System.out.println("mm.add(3, 3L) 결과:"	 + mm.add(3, 3L));
+		System.out.println("mm.add(3L, 3) 결과:"	 + mm.add(3L, 3));
+		System.out.println("mm.add(3L, 3L) 결과:"	 + mm.add(3L, 3L));
+
+		int[] a = {100, 200, 300};
+		System.out.println("mm.add(a) 결과:" + mm.add(a));
 
 	}
 
+}
+
+class Math3 {
+	int add (int a, int b) {
+		System.out.print("int add(int a, int b) - ");
+		return a + b;
+//		result = a + b;
+//		return result;
+	}
+	
+	long add(int a, long b) {
+		System.out.print("long add(int a, long b) - ");
+		return a + b;
+	}
+	
+	long add(long a, int b) {
+		System.out.print("int add(long a, int b) - ");
+		return a + b;
+	}
+	
+	long add(long a, long b) {
+		System.out.print("int add(long a, long b) - ");
+		return a + b;
+	}
+	
+	int add(int [] a) {
+		System.out.print("int add(int a, int b) - ");
+		int result = 0;
+		for (int i = 0; i < a.length; i++) {
+			result += a[i];
+		}
+		return result;
+	}
+	
 }
---
+++ ch06/src/ThisMethod_This.java
@@ -0,0 +1,43 @@
+/*
+ this()
+ 생성자의 이름으로 클래스이름 대신 this를 사용한다.
+ 생성자에서 다른 생성자 호출할 때 사용
+ 다른 생성자 호출할 때는 반드시 첫 줄에서만 호출이 가능하다.
+ */
+
+public class ThisMethod_This {
+
+	public static void main(String[] args) {
+		Car2 c1 = new Car2();
+		Car2 c2 = new Car2("blue");
+		
+		
+		
+		System.out.println("c1의 color=" + c1.color + ", c1의 gearType=" + c1.gearType + ", c1의 door=" + c1.door);
+		System.out.println("c2의 color=" + c2.color + ", c2의 gearType=" + c2.gearType + ", c2의 door=" + c2.door);
+	}
+}
+
+
+
+class Car2{
+	String color;
+	String gearType;
+	int door;
+	
+	Car2(){
+		this("white", "auto", 4);		// Car(String color, String gearType, int door)를 호출
+	}
+	
+	Car2(String color){
+		this(color, "auto", 4);
+	}
+	
+	Car2(String color, String gearType, int door){
+		this.color = color;				// this.~ 인스턴스변수, color은 지역변수
+		this.gearType = gearType;
+		this.door = door;
+		
+	
+	}
+}	
